bf8b0631dc8b1eb0e932413f2b2125fc6f68d8f5
Sergey Gromakov <sisolin@icloud.com>
2023-04-05 17:19:03 +0300

    Added correct 36th Task

--- Hometask7/Task36.py
+++ Hometask7/Task36.py
@@ -5,7 +5,7 @@
 # почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
 # ровно два аргумента, как, например, у операции умножения.
 # Ввод: Вывод:
-# print_operation_table(lambda x, y: x * y) 
+# print_operation_table(lambda x, y: x * y)
 # 1 2 3 4 5 6
 #  2 4 6 8 10 12
 #  3 6 9 12 15 18
@@ -13,10 +13,13 @@
 #  5 10 15 20 25 30
 #  6 12 18 24 30 36
 
-def print_operation_table(operation, num_rows, num_сolumns):
-    arr=[[operation(i,j) for i in range(1,num_rows+1)] for j in range(1, num_сolumns+1)]
-    for i in arr:
-        print(*[f"{x:>3}"for x in i])
+def print_operation_table(operation, num_rows, num_columns):
+    for i in range(1, num_rows+1):
+        arr = []
+        for j in range(1, num_columns+1):
+            arr.append(str(operation(i, j)))
+        print(''.join(f'{e:<4}' for e in arr))
+
 line = int(input("Введите количество строк: "))
 columns = int(input("Введите количество столбцов: "))
-print_operation_table(lambda x,y: x*y,line,columns)
+print_operation_table(lambda x, y: x*y,line,columns)
